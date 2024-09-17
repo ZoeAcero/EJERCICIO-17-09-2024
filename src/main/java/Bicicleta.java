e2281f7a7c0f073b663e6b73be490f09c0f8a816
ZoeAcero <zar.amazona@gmail.com>
2024-09-17 18:13:15 +0200

    a

--- src/main/java/Bicicleta.java
+++ src/main/java/Bicicleta.java
@@ -1,2 +1,11 @@
 public class Bicicleta {
+    private int VelocidadActual;
+    private int platoActual;
+    private int piñonActual;
+
+    public Bicicleta(int VelocidadActual, int platoActual, int piñonActual){
+        this.VelocidadActual = VelocidadActual;
+        this.platoActual = platoActual;
+        this.piñonActual = piñonActual;
+    }
 }
