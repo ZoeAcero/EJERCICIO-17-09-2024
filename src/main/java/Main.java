366ba7c4a8fc5205bbeb3f0b985bc809afc3814e
ZoeAcero <zar.amazona@gmail.com>
2024-09-17 18:25:04 +0200

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Bicicleta bicicleta = new Bicicleta(10, 1, 1);
+        bicicleta.acelerar();
+        bicicleta.frenar();
+        bicicleta.cambiarPlato(2);
+        bicicleta.cambiarPiñon(2);
+    }
 }
